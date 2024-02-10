0fc851584a10f23cc0743241e37aaccb5246320b
severino gouveia <severinogouveia_@hotmail.com>
2024-02-10 08:12:24 -0300

    v1.8

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.form.upload import ImageUploadField
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dados.db'
@@ -13,21 +14,29 @@ app.config['SECRET_KEY'] = '2722'
 # Configuração para upload de imagens
 app.config['UPLOAD_FOLDER'] = 'static/img/'
 
+# Configuração do Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'severinogouveia59@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tnum eqeu oetl zkpt'
+app.config['MAIL_DEFAULT_SENDER'] = 'severinogouveia59@gmail.com'
+
+mail = Mail(app)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Define o modelo para os itens do carrossel
 class CarouselItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image_filename = db.Column(db.String(100), nullable=False)
     caption = db.Column(db.String(200))
     link_text = db.Column(db.String(100))
-    team_type = db.Column(db.String(50))  # Adicionando o tipo de equipe
+    team_type = db.Column(db.String(50))
     link_url = db.Column(db.String(200))
 
-# Define a visualização para o Flask-Admin
 class CarouselItemAdmin(ModelView):
-    form_columns = ['image_filename', 'caption','link_text', 'team_type', 'link_url']
+    form_columns = ['image_filename', 'caption', 'link_text', 'team_type', 'link_url']
     
     form_extra_fields = {
         'image_filename': ImageUploadField('Image',
@@ -42,31 +51,33 @@ class Resposta(db.Model):
     cantar = db.Column(db.Integer)
     receber_email = db.Column(db.Integer)
     email = db.Column(db.String(100))
-    evento = db.Column(db.String(100))  # Adicione o campo evento
+    evento = db.Column(db.String(100))
 
     carousel_item_id = db.Column(db.Integer, db.ForeignKey('carousel_item.id', name='fk_resposta_carousel_item'))
     carousel_item = db.relationship('CarouselItem', backref=db.backref('respostas', lazy=True))
 
-
-
-# Define a visualização para o Flask-Admin para o modelo Resposta
 class RespostaAdmin(ModelView):
     form_columns = ['nome', 'tocar', 'cantar', 'receber_email', 'email', 'evento', 'carousel_item']
 
-# Registra os modelos e as visualizações no Flask-Admin
+class MensagemEmail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    assunto = db.Column(db.String(100), nullable=False)
+    mensagem = db.Column(db.Text, nullable=False)
+
+class MensagemEmailAdmin(ModelView):
+    column_list = ['nome', 'email', 'assunto', 'mensagem']
+    form_columns = ['nome', 'email', 'assunto', 'mensagem']
+
 admin = Admin(app, name='Painel de Administração', template_mode='bootstrap3')
 admin.add_view(CarouselItemAdmin(CarouselItem, db.session))
 admin.add_view(RespostaAdmin(Resposta, db.session))
-
-from flask import request
+admin.add_view(MensagemEmailAdmin(MensagemEmail, db.session))
 
 @app.route('/')
 def index():
     respostas = Resposta.query.all()
-    print("Respostas encontradas:")
-    for resposta in respostas:
-        print(f"Nome: {resposta.nome}, Evento: {resposta.evento}, Tocar: {resposta.tocar}, Cantar: {resposta.cantar}")  
-        # Imprime o nome, evento, tocar e cantar de cada resposta
     carousel_items = CarouselItem.query.all()
     team_types = set([item.team_type for item in carousel_items])
     return render_template('index.html', respostas=respostas, carousel_items=carousel_items, team_types=team_types)
@@ -78,33 +89,95 @@ def enviar():
     cantar = 1 if 'cantar' in request.form else 0
     receber_email = 1 if 'receberEmailCheckbox' in request.form else 0
     email = request.form['email'] if receber_email else None
-
     evento = request.form.get('domingo')  
-
     carousel_item_id = request.form.get('carousel_item_id')
+
+    # Recuperando o objeto CarouselItem correspondente ao carousel_item_id
+    carousel_item = CarouselItem.query.get(carousel_item_id)
+
+    # Envio do email de confirmação
+    if receber_email and email and carousel_item:
+        msg = Message('Confirmação de Registro',
+                      sender='severinogouveia59@gmail.com',
+                      recipients=[email])
+        msg.body = f'Olá {nome}, obrigado por se registrar para a {evento} no dia {carousel_item.caption} | {carousel_item.team_type}!'
+
+        mail.send(msg)
 
     resposta = Resposta(nome=nome, tocar=tocar, cantar=cantar, receber_email=receber_email, email=email, evento=evento, carousel_item_id=carousel_item_id)  # Adiciona o evento à resposta
     db.session.add(resposta)
     db.session.commit()
 
+
     return redirect(url_for('index'))
+
+@app.route('/enviar_email', methods=['POST'])
+def enviar_email():
+    nome = request.form['nome']
+    email = request.form['email']
+    assunto = request.form['assunto']
+    mensagem = request.form['mensagem']
+
+    # Configuração da mensagem de e-mail
+    msg = Message(assunto, sender=email, recipients=['severinogouveia59@gmail.com'])
+    msg.body = f'De: {nome}\nEmail: {email}\n\n{mensagem}'
+
+    # Envio do e-mail
+    try:
+        mail.send(msg)
+        return redirect('/thanks')  # Redireciona para a rota '/thanks' após o envio bem-sucedido
+    except Exception as e:
+        return str(e)
+
 
 @app.route('/thanks')
 def thanks():
     return render_template('thanks.html')
 
-
-
 @app.route('/form.html/<domingo>', methods=['GET', 'POST'])
 def form(domingo):
     if request.method == 'POST':
-        return redirect(url_for('enviar', domingo=domingo))  # Passa o valor de domingo para a função enviar()
-
+        return redirect(url_for('enviar', domingo=domingo))
  
-    # Por exemplo:
     carousel_item = CarouselItem.query.filter_by(link_text=domingo).first()
 
     return render_template('form.html', domingo=domingo, carousel_item=carousel_item)
 
+@app.route('/enviar_formulario_email', methods=['POST'])
+def enviar_formulario_email():
+    nome = request.form['nome']
+    email = request.form['email']
+    assunto = request.form['assunto']
+    mensagem = request.form['mensagem']
+
+    # Salvar as informações no banco de dados
+    try:
+        nova_mensagem = MensagemEmail(nome=nome, email=email, assunto=assunto, mensagem=mensagem)
+        db.session.add(nova_mensagem)
+        db.session.commit()
+
+        # Imprimir as informações no terminal
+        print("Nome:", nome)
+        print("Email:", email)
+        print("Assunto:", assunto)
+        print("Mensagem:", mensagem)
+
+        # Configuração da mensagem de e-mail
+        msg = Message(assunto, sender=email, recipients=['severinogouveia59@gmail.com'])
+        msg.body = f'De: {nome}\nEmail: {email}\n\n{mensagem}'
+
+        # Envio do e-mail
+        try:
+            mail.send(msg)
+            print("E-mail enviado com sucesso de:", email)
+            return redirect('/thanks')  # Redireciona para a rota '/thanks' após o envio bem-sucedido
+        except Exception as e:
+            print("Erro ao enviar e-mail:", str(e))
+            return str(e)
+
+    except Exception as e:
+        return redirect('/thanks')  # Redireciona para a rota '/thanks' após o envio bem-sucedido
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
